88b77565942fd569fb0378197b58cecdf2e9ede3
IngEnigma <159200281+IngEnigma@users.noreply.github.com>
2025-03-27 11:09:16 -0600

    Update crime.py

--- crime.py
+++ crime.py
@@ -26,24 +26,24 @@ if __name__ == "__main__":
                FROM crimes WHERE victim_sex = 'M' 
                ORDER BY report_date"""
     df_male_crimes = spark.sql(query)
-    df_male_crimes.write.csv("results/male_crimes.csv", header=True, mode="overwrite")
+    df_male_crimes.write.json("results/male_crimes", mode="overwrite")
 
     query = '''SELECT DR_NO, report_date, victim_age, victim_sex, `Crm Cd Desc` 
                FROM crimes WHERE report_date BETWEEN '2019-01-01' AND '2020-12-31' 
                ORDER BY report_date'''
     df_crimes_2019_2020 = spark.sql(query)
-    df_crimes_2019_2020.write.csv("results/crimes_2019_2020.csv", header=True, mode="overwrite")
+    df_crimes_2019_2020.write.json("results/crimes_2019_2020", mode="overwrite")
 
     query = '''SELECT AREA, COUNT(AREA) as crime_count 
                FROM crimes 
                GROUP BY AREA ORDER BY crime_count DESC'''
     df_crimes_by_area = spark.sql(query)
-    df_crimes_by_area.write.csv("results/crimes_by_area.csv", header=True, mode="overwrite")
+    df_crimes_by_area.write.json("results/crimes_by_area", mode="overwrite")
 
     query = '''SELECT DR_NO, report_date, victim_age, victim_sex, `Crm Cd Desc` 
                FROM crimes WHERE victim_age BETWEEN 18 AND 30 
                ORDER BY victim_age'''
     df_young_adults_crimes = spark.sql(query)
-    df_young_adults_crimes.write.csv("results/young_adults_crimes.csv", header=True, mode="overwrite")
+    df_young_adults_crimes.write.json("results/young_adults_crimes", mode="overwrite")
 
     spark.stop()
